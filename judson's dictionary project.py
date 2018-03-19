3fc3d22d7ff1cbc379455110f90fe72c8c0478af
Tabor <judson.tabor@stu.jefferson.kyschools.us>
2018-03-19 08:43:07 -0400

    judson's dictionary

--- judson's dictionary project.py
+++ judson's dictionary project.py
@@ -1,6 +1,34 @@
-Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
- RESTART: //jcpsfs/students/jmtabo02/My Documents/judson's dictionary project.py 
-{'luke Skywalker': 'Offspring of Anakin Skywalker and the last jedi', 'Leia Organa': "luke's sister and leader of the rebel alliance", 'han solo': 'orphan who grew up to be a smuggler and hero of the rebel alliance', 'chewbacca': 'wookie warrior who fought in the clone wars and galactic civil war', 'Obi Wan': 'jedi master who fought in the clone wars and trained Anakin and luke Skywalker', 'Darth Vader': 'a sith lord who ruled the galaxy with an iron fist with his master', 'Darth sidious': ' a sith master who fooled the whole galaxy into his rule', 'storm troopers': 'the loyal soldiers of the galactic empire', 'boba fett': 'ruthless bounty hunter that works for the empire', 'grand moff tarkin': "the empire's most experinced and powerful general", 'wedge antilles': " the rebel alliance's most experinced pilot and leader of rogue squadron"}
->>> 
+#judson tabor's dictiornary
+
+(Star_wars_characters) = {
+
+    "luke Skywalker": "Offspring of Anakin Skywalker and the last jedi",
+
+    "Leia Organa": "luke's sister and leader of the rebel alliance",
+
+    "han solo": "orphan who grew up to be a smuggler and hero of the rebel alliance",
+
+    "chewbacca": "wookie warrior who fought in the clone wars and galactic civil war",
+
+    "Obi Wan": "jedi master who fought in the clone wars and trained Anakin and luke Skywalker",
+
+    "Darth Vader": "a sith lord who ruled the galaxy with an iron fist with his master",
+
+    "Darth sidious": " a sith master who fooled the whole galaxy into his rule",
+
+    "storm troopers": "the loyal soldiers of the galactic empire",
+
+    "boba fett": "ruthless bounty hunter that works for the empire",
+
+    "grand moff tarkin": "the empire's most experinced and powerful general",
+
+    "wedge antilles": " the rebel alliance's most experinced pilot and leader of rogue squadron"         }
+
+print (Star_wars_characters)
+
+
+
+
+    
+
+ 
